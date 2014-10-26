c97aa636da0b031779967d16ba4f5f024c496dcb
Pyrax <piranhadev@gmail.com>
2014-10-26 23:04:11 +0100

    Add compilation for filterscripts

--- bin/compile.bat
+++ bin/compile.bat
@@ -1,7 +1,10 @@
 @echo off
 setlocal enableDelayedExpansion 
 
-@echo on
 for /F %%G in ('dir /B/D ..\src\gamemodes\*.pwn') do (
     "compiler\pawncc.exe" "..\src\gamemodes\%%G" -o"..\src\gamemodes\%%~nG.amx" -i"..\src\includes" "-;" "-("
+)
+
+for /F %%G in ('dir /B/D ..\src\filterscripts\*.pwn') do (
+    "compiler\pawncc.exe" "..\src\filterscripts\%%G" -o"..\src\filterscripts\%%~nG.amx" -i"..\src\includes" "-;" "-("
 )
