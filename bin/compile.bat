7eee9d4d7c8d2461d08892dc8b547016e231f4db
Pyrax <piranhadev@gmail.com>
2014-10-27 16:39:35 +0100

    Add ECHO messages

--- bin/compile.bat
+++ bin/compile.bat
@@ -1,10 +1,14 @@
 @echo off
 setlocal enableDelayedExpansion
 
+Echo Compiling gamemodes...
 for /F %%G in ('dir /B/D ..\src\gamemodes\*.pwn') do (
+    Echo src\gamemodes\%%~nxG
     "compiler\pawncc.exe" "..\src\gamemodes\%%G" -o"server\gamemodes\%%~nG.amx" -i"..\src\includes" "-;" "-("
 )
 
+Echo Compiling filterscripts...
 for /F %%G in ('dir /B/D ..\src\filterscripts\*.pwn') do (
+    Echo src\filterscripts\%%~nxG
     "compiler\pawncc.exe" "..\src\filterscripts\%%G" -o"server\filterscripts\%%~nG.amx" -i"..\src\includes" "-;" "-("
 )
