ebf6524b8dffb3e26238a252ded929ca2e9ef88f
Pyrax <piranhadev@gmail.com>
2014-10-27 15:06:54 +0100

    Change directory structure, add bin/server

--- bin/compile.bat
+++ bin/compile.bat
@@ -1,10 +1,10 @@
 @echo off
-setlocal enableDelayedExpansion 
+setlocal enableDelayedExpansion
 
 for /F %%G in ('dir /B/D ..\src\gamemodes\*.pwn') do (
-    "compiler\pawncc.exe" "..\src\gamemodes\%%G" -o"..\src\gamemodes\%%~nG.amx" -i"..\src\includes" "-;" "-("
+    "compiler\pawncc.exe" "..\src\gamemodes\%%G" -o"server\gamemodes\%%~nG.amx" -i"..\src\includes" "-;" "-("
 )
 
 for /F %%G in ('dir /B/D ..\src\filterscripts\*.pwn') do (
-    "compiler\pawncc.exe" "..\src\filterscripts\%%G" -o"..\src\filterscripts\%%~nG.amx" -i"..\src\includes" "-;" "-("
+    "compiler\pawncc.exe" "..\src\filterscripts\%%G" -o"server\filterscripts\%%~nG.amx" -i"..\src\includes" "-;" "-("
 )
