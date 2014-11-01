add691b3d3a8ebea38508155e3105c7782f56fc7
Pyrax <piranhadev@gmail.com>
2014-11-01 16:25:46 +0100

    Rewrite deploy.js with npm module ftp-deploy

--- bin/appveyor/deploy.js
+++ bin/appveyor/deploy.js
@@ -1,11 +1,49 @@
 /**
  * Created by Piranha on 31.10.2014.
  */
-var Client = require('ftp');
+var Client = require('ftp-deploy');
 var fs = require('fs');
 var path = require('path');
 
 var ftp = new Client();
+
+var config = {
+    host: process.env.samp_ftp_server,
+    port: 21,
+    username: process.env.samp_ftp_user,
+    password: process.env.samp_ftp_password,
+    continueOnError: true
+};
+
+config.localRoot = '../server/gamemodes';
+config.remoteRoot = 'gamemodes';
+
+ftp.deploy(config , function(err) {
+    if(err) throw err;
+    else console.log('Gamemodes deployed...');
+});
+
+config.localRoot = '../server/filterscripts';
+config.remoteRoot = 'filterscripts';
+
+ftp.deploy(config, function(err) {
+    if(err) throw err;
+    else console.log('Filterscripts deployed...');
+});
+
+config.localRoot = '../server/plugins';
+config.remoteRoot = 'plugins';
+
+ftp.deploy(config, function(err) {
+    if(err) throw err;
+    else console.log('Plugins deployed...');
+});
+
+ftp.on('upload-error', function (data) {
+    console.log(data.err);
+});
+
+/*var ftp = new Client();
 
 ftp.on('ready', DeployFiles);
 ftp.on('error', Error);
@@ -23,6 +61,16 @@ function DeployFiles() {
             if(err) throw err;
         });
     });
+    
+    var uploadQueue = IterateFiles('..\\server');
+    var itemsUploaded;
+    
+    uploadQueue.forEach(function(file)) {
+        console.log('Trying to upload ' + file + '\nto ' + path.resolve(file, '..'));
+        ftp.put(file, path.resolve(file, '..'), function(err) {
+            if(err) throw err;
+        });
+    }
 }
 
 function Error(err) {
@@ -51,7 +99,20 @@ function IterateFiles(dir, cb) {
     });
 }
 
-process.on('exit', function(code) {
-    // make sure FTP connection gets closed
-    ftp.end();
-});
+function IterateFiles(dir) {
+    var output = [];
+    
+    var files = fs.readdirSync(dir);
+    files.forEach(function(file) {
+        file = path.resolve(dir, file);
+        var stats = fs.statSync(dir);
+        
+        if(stats.isDirectory()) {
+            IterateFiles(dir);
+        } else if(stats.isFile()) {
+            output.push(file);
+        }
+    });
+    
+    return output;
+}*/
