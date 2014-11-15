b5ce38a5e36971a5cdc8db724a0d91b229da78d3
Pyrax <piranhadev@gmail.com>
2014-11-15 17:38:59 +0100

    Test new ftp module

--- bin/appveyor/deploy.js
+++ bin/appveyor/deploy.js
@@ -1,48 +1,55 @@
 /**
  * Created by Piranha on 31.10.2014.
  */
-var Client = require('ftp-deploy');
+var Client = require('ftp-client');
 var fs = require('fs');
 var path = require('path');
-
-var ftp = new Client();
 
 var config = {
     host: process.env.samp_ftp_server,
     port: 21,
-    username: process.env.samp_ftp_user,
-    password: process.env.samp_ftp_password,
-    continueOnError: true
+    user: process.env.samp_ftp_user,
+    password: process.env.samp_ftp_password
 };
+
+var ftp = new Client(config, {
+    overwrite: all
+});
 
 config.localRoot = '../server/gamemodes';
 config.remoteRoot = 'gamemodes';
 
-ftp.deploy(config , function(err) {
-    if(err) console.log(err);
-    else {
-        console.log('Gamemodes deployed...');
-        
-        config.localRoot = '../server/filterscripts';
-        config.remoteRoot = 'filterscripts';
+ftp.connect(function() {
+    /*ftp.deploy(config , function(err) {
+        if(err) console.log(err);
+        else {
+            console.log('Gamemodes deployed...');
+            
+            config.localRoot = '../server/filterscripts';
+            config.remoteRoot = 'filterscripts';
 
-        ftp.deploy(config, function(err) {
-            if(err) console.log(err);
-            else {
-                console.log('Filterscripts deployed...');
-                
-                config.localRoot = '../server/plugins';
-                config.remoteRoot = 'plugins';
+            ftp.deploy(config, function(err) {
+                if(err) console.log(err);
+                else {
+                    console.log('Filterscripts deployed...');
+                    
+                    config.localRoot = '../server/plugins';
+                    config.remoteRoot = 'plugins';
 
-                ftp.deploy(config, function(err) {
-                    if(err) console.log(err);
-                    else console.log('Plugins deployed...');
-                });
-            }
-        });
-    }
+                    ftp.deploy(config, function(err) {
+                        if(err) console.log(err);
+                        else console.log('Plugins deployed...');
+                    });
+                }
+            });
+        }
+    });*/
+    
+    ftp.upload(['../server/gamemodes/**'], '/gamemodes', {}, function(result) {
+        console.log('Gamemodes deployed... (' + result + ')');
+    });
 });
 
-ftp.on('upload-error', function (data) {
+/*ftp.on('upload-error', function (data) {
     console.log(data.err);
-});
+});*/
